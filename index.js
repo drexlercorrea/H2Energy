6c3b8d90c1b2edcc99249b406b2427db6d46d476
drexlercorrea <drexlercorrea@hotmail.com>
2023-04-18 10:38:35 -0300

    Correção da comunicação com o servidor

--- index.js
+++ index.js
@@ -34,75 +34,51 @@ function campoVazio() {
 /* ENVIO DO FORMULÁRIO PARA AIRTABLE */
 function enviarFormulario() {
   var fcv = campoVazio();
-
-  if (fcv == 2) {
+  if (fcv !== 2) {
+    alert("Preencha todos os campos!");
+  } else {
+    var fNome = ""; var fPotencia = ""; var fCidade = ""; var fUsinas = ""; var fStatus = ""; var fData = "";
     var dataAtual = new Date();
-    var dia = dataAtual.getDate();
-    var mes = dataAtual.getMonth() + 1;
-    var ano = dataAtual.getFullYear();
-    var dataFormatada =
-      (dia < 10 ? "0" : "") +
-      dia +
-      "/" +
-      (mes < 10 ? "0" : "") +
-      mes +
-      "/" +
-      ano;
+    var dia = dataAtual.getDate(); var mes = dataAtual.getMonth() + 1; var ano = dataAtual.getFullYear();
+    var dataFormatada = (dia < 10 ? "0" : "") + dia + "/" + (mes < 10 ? "0" : "") + mes + "/" + ano;
     var p1 = document.getElementById("projeto1");
-    var ep1 = document.getElementById("projeto1").childElementCount;
-    var nid = ep1 / 6 + 1;
-    var fId =
-      (mes < 10 ? "0" : "") +
-      mes +
-      (dia < 10 ? "0" : "") +
-      dia +
-      (nid < 10 ? "0" : "") +
-      nid +
-      (ano - 2000);
+    var ep1 = document.getElementById("projeto1").childElementCount; var nid = ep1 / 6 + 1;
+    var fId = (mes < 10 ? "0" : "") + mes + (dia < 10 ? "0" : "") + dia + (nid < 10 ? "0" : "") + nid + (ano - 2000);
     var fNome = document.getElementById("nome").value;
     var fPotencia = document.getElementById("potencia").value;
     var fCidade = document.getElementById("cidade").value;
     var fUsinas = document.getElementById("usinas").value;
     var fStatus = document.getElementById("status").value;
-    var fData = dataAtual;
+    var fData = dataFormatada;
 
     var fc = document.getElementById("formCadastro");
     fc.addEventListener("submit", async (event) => {
       event.preventDefault();
       var dadosEnvio = {
         fields: {
-          ID: fId,
-          "Nome da UFV": fNome,
-          Potência: fPotencia,
-          "Cidade/UF": fCidade,
-          "Nº de usinas": fUsinas,
-          Status: fStatus,
-          Data: fData,
+          "id": fId, "nomedaufv": fNome, "potencia": fPotencia, "cidadeuf": fCidade, "ndeusinas": fUsinas, "status": fStatus, "data": fData,
         },
       };
-
-      try {
-        const response = await fetch(
-          "https://api.airtable.com/v0/pat7sOvlibq6Ui1sG/projetosh2",
-          {
-            method: "POST",
-            headers: {
-              Authorization: "Bearer keyggOsGLubPoGKDd",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(dadosEnvio),
-          }
-        );
-        console.log(
-          "Dados enviados com sucesso para o Airtable:",
-          response.data
-        );
-      } catch (error) {
-        console.error("Erro ao enviar dados para o Airtable:", error);
+      if (fcv == 2) {
+        try {
+          const response = await fetch("https://api.airtable.com/v0/app9EDXVbU7QhtUiF/projetosh2",
+            {
+              method: "POST",
+              headers: {
+                Authorization: "Bearer keyggOsGLubPoGKDd",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(dadosEnvio),
+            }
+          );
+          console.log("Dados enviados com sucesso para o Airtable:", response.data);
+        } catch (error) {
+          console.error("Erro ao enviar dados para o Airtable:", error);
+        }
       }
     });
 
-    /*  var id = document.createElement("h5");
+    /* var id = document.createElement("h5");
     id.innerText = (mes < 10 ? "0" : "") + mes + (dia < 10 ? "0" : "") + dia + (nid < 10 ? "0" : "") + nid + (ano - 2000);
     p1.appendChild(id);
 
@@ -134,7 +110,7 @@ function enviarFormulario() {
     document.getElementById("status").value = "";
 
     abrirFormulario();
-  } else {
-    alert("Preencha todos os campos!");
   }
 }
+
+
