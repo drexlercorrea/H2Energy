b44bb348d0e2e2e1ec6d883b51473e9dc629af72
drexlercorrea <drexlercorrea@hotmail.com>
2023-04-19 19:24:59 -0300

    Função para tratar dados

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* HEAD */
 const Airtable = require("airtable");
 const base = new Airtable({ apiKey: "keyggOsGLubPoGKDd" }).base("projetosh2");
+document.addEventListener("DOMContentLoaded", function() {var dadosEnviados = 1; receberDados();});
+
+/* TRATAMENTO DOS DADOS RECEBIDOS */
+function tratarDados() {
+  var idpAirtable = fieldsAirtable.idprojeto;
+  var nomeAirtable = fieldsAirtable.nomedaufv;
+  var potenciaAirtable = fieldsAirtable.potencia;
+  var cidadeAirtable = fieldsAirtable.cidadeuf;
+  var statusAirtable = fieldsAirtable.status;
+  var dataAirtable = fieldsAirtable.data;
+
+  var idp = document.createElement("h5");
+  idp.innerText = idpAirtable;
+  p1.appendChild(idp);
+
+  var nome = document.createElement("a");
+  nome.innerText = nomeAirtable;
+  nome.setAttribute("class", "ufv"); nome.setAttribute("href", "#");
+  p1.appendChild(nome);
+
+  var potencia = document.createElement("h5");
+  potencia.innerText = potenciaAirtable;
+  p1.appendChild(potencia);
+
+  var cidade = document.createElement("h5");
+  cidade.innerText = cidadeAirtable;
+  p1.appendChild(cidade);
+
+  var status = document.createElement("h5");
+  status.innerText = statusAirtable;
+  p1.appendChild(status);
+
+  var data = document.createElement("h5");
+  data.innerText = dataAirtable;
+  p1.appendChild(data); 
+}
 
 /* OBTER DADOS CADASTRADOS NO AIRTABLE */
 function receberDados() {
@@ -21,40 +57,17 @@ function receberDados() {
     .then(data => {
       var p1 = document.getElementById("projeto1");
       var dadosAirtable = data.records;
-      for (let i = 0; i < dadosAirtable.length; i++) {
-        var fieldsAirtable = dadosAirtable[i].fields;
-        var idpAirtable = fieldsAirtable.idprojeto;
-        var nomeAirtable = fieldsAirtable.nomedaufv;
-        var potenciaAirtable = fieldsAirtable.potencia;
-        var cidadeAirtable = fieldsAirtable.cidadeuf;
-        var statusAirtable = fieldsAirtable.status;
-        var dataAirtable = fieldsAirtable.data;
-
-        var idp = document.createElement("h5");
-        idp.innerText = idpAirtable;
-        p1.appendChild(idp);
-    
-        var nome = document.createElement("a");
-        nome.innerText = nomeAirtable;
-        nome.setAttribute("class", "ufv"); nome.setAttribute("href", "#");
-        p1.appendChild(nome);
-      
-        var potencia = document.createElement("h5");
-        potencia.innerText = potenciaAirtable;
-        p1.appendChild(potencia);
-      
-        var cidade = document.createElement("h5");
-        cidade.innerText = cidadeAirtable;
-        p1.appendChild(cidade);
-      
-        var status = document.createElement("h5");
-        status.innerText = statusAirtable;
-        p1.appendChild(status);
-      
-        var data = document.createElement("h5");
-        data.innerText = dataAirtable;
-        p1.appendChild(data); 
-      }
+      if (dadosEnviados == 1) {
+        for (let i = 0; i < dadosAirtable.length; i++) {
+          var fieldsAirtable = dadosAirtable[i].fields;
+          tratarDados ();
+        }
+      } else {
+        var ultimoEnviado = dadosAirtable.length - 1;
+        var fieldsAirtable = dadosAirtable[ultimoEnviado].fields;
+        tratarDados ();        
+        dadosEnviados = "";
+      }      
     })
     .catch(error => {
       console.error(error);
@@ -99,7 +112,7 @@ function enviarFormulario() {
     var dataAtual = new Date();
     var dia = dataAtual.getDate(); var mes = dataAtual.getMonth() + 1; var ano = dataAtual.getFullYear();
     var dataFormatada = (dia < 10 ? "0" : "") + dia + "/" + (mes < 10 ? "0" : "") + mes + "/" + ano;
-    var ep1 = document.getElementById("projeto1").childElementCount; var nid = (ep1 -1) / 6 + 1;
+    var ep1 = document.getElementById("projeto1").childElementCount; var nid = ep1 / 6 + 1;
     var fId = (mes < 10 ? "0" : "") + mes + (dia < 10 ? "0" : "") + dia + (nid < 10 ? "0" : "") + nid + (ano - 2000);
     var fNome = document.getElementById("nome").value;
     var fPotencia = document.getElementById("potencia").value;
@@ -128,6 +141,7 @@ function enviarFormulario() {
           body: JSON.stringify(dadosEnvio),            
         });          
         console.log("Dados enviados com sucesso para o Airtable:", response.data);
+        var dadosEnviados = 2;
         receberDados();   
       } catch (error) {        
         console.error("Erro ao enviar dados para o Airtable:", error);        
